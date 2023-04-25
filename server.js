243cb27ca82bcc9c838b654bc9baf10aa72c9576
Rexedge <iscedigitalconcept@gmail.com>
2023-04-25 18:08:18 +0100

    modifying htaccess

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-const express = require('express')
-const next = require('next')
+// const express = require('express')
+// const next = require('next')
 
-const app = express()
+// const app = express()
 
-const dev = process.env.NODE_ENV !== 'production'
-const nextApp = next({ dev })
+// const dev = process.env.NODE_ENV !== 'production'
+// const nextApp = next({ dev })
 
-const publicHandler = express.static('public')
+// const publicHandler = express.static('public')
 
-const defaultHandler = nextApp.getRequestHandler()
+// const defaultHandler = nextApp.getRequestHandler()
 
-nextApp.prepare().then(() => {
-     app.use('/public', publicHandler)
-     app.use(defaultHandler)
-     app.listen(3000, err => {
-          if (err) throw err
-          console.log('> Ready on http://localhost:3000')
-     })
-})
+// nextApp.prepare().then(() => {
+//      app.use('/public', publicHandler)
+//      app.use(defaultHandler)
+//      app.listen(3000, err => {
+//           if (err) throw err
+//           console.log('> Ready on http://localhost:3000')
+//      })
+// })
